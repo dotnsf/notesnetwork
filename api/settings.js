adacdcee6f5b13b6bbbc5cd5db88180cd109b1be
root <root@VM-aa499645-dc4a-459e-8835-dc07a089e211.cs6bidcfcloud.internal>
2018-03-23 15:14:15 +0900

    api/settings.js updated

--- api/settings.js
+++ api/settings.js
@@ -4,5 +4,5 @@ exports.basic_username = 'username';
 exports.basic_password = 'password';
 
 exports.cloudant_db = 'notesnetwork'; //. Use cloudant if this value is not blank
-exports.cloudant_username = '74b8201c-dd3d-42d0-8cd2-81f4085f2aac-bluemix';
-exports.cloudant_password = 'b3b0fbbd8a6a217cf1468c82ad452ca408f64b6075764a121c10cb05f6f53bd8';
+exports.cloudant_username = '';
+exports.cloudant_password = '';
