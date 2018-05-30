2f1792cb1c9b2477d920077711f22986333fc887
root <root@VM-aa499645-dc4a-459e-8835-dc07a089e211.cs6bidcfcloud.internal>
2018-05-30 18:01:06 +0900

    set cloudant_db as blank in default

--- api/settings.js
+++ api/settings.js
@@ -3,6 +3,6 @@ exports.cardName = 'admin@notes-network'; //. 'PeerAdmin@hlfv1';
 exports.basic_username = 'username';
 exports.basic_password = 'password';
 
-exports.cloudant_db = 'notesnetwork'; //. Use cloudant if this value is not blank
+exports.cloudant_db = ''; //. Use cloudant if this value is not blank
 exports.cloudant_username = '';
 exports.cloudant_password = '';
