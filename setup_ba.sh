1db52dde452427c174161b54c5e5dfc5cee66029
root <root@VM-aa499645-dc4a-459e-8835-dc07a089e211.cs6bidcfcloud.internal>
2018-05-30 17:53:14 +0900

    package.json fixed. setup_ba.sh refined.

--- setup_ba.sh
+++ setup_ba.sh
@@ -2,7 +2,9 @@
 
 composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName notes-network
 
-composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile ./api/notes-network.bna --file ~/PeerAdmin@hlfv1.card
+composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile ./api/notes-network.bna --file ~/admin@notes-network.card
+
+composer card import --file ~/admin@notes-network.card
 
 composer network ping --card admin@notes-network
 
